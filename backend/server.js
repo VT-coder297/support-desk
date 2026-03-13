2545a953d0a5cb114d17d5e6747e88dc21097cb7
VT-coder297 <tmot45548@gmail.com>
2026-03-13 17:53:52 +0200

    4

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,47 @@
+const path = require('path');
 const express = require('express');
-const colors = require('colors');
-const dotenv = require('dotenv').config();
+require('colors');
+require('dotenv').config();
 const {
   errorHandler,
   invalidPathHandler,
 } = require('./middleware/errorMiddleware');
 const connectDB = require('./config/db');
 const PORT = process.env.PORT || 5000;
+const app = express();
 
 // Connect to database
 connectDB();
 
-const app = express();
-
+// // Parse "application/json" incoming Request objects as a JSON Object
 app.use(express.json());
+// // Parse "application/x-www-form-urlencoded", can only parse incoming Request Object if strings or arrays
 app.use(express.urlencoded({ extended: false }));
+// combines the 2 above, can parse objects with nested objects, and generally any type
+// app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'API is running' });
-});
-
-// Import and use user routes
+// Routes
 app.use('/api/users', require('./routes/userRoutes'));
-// Import and use ticket routes
 app.use('/api/tickets', require('./routes/ticketRoutes'));
+
+// Serve Frontend
+if (process.env.NODE_ENV === 'production') {
+  // Set build folder as static
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+  // FIX: below code fixes app crashing on refresh in deployment
+  app.get('*', (_, res) =>
+    res.sendFile(
+      path.join(__dirname, '../', 'frontend', 'build', 'index.html'),
+    ),
+  );
+} else {
+  app.get('/', (_, res) => {
+    res.status(200).json({ message: 'Welcome to the Support Desk API' });
+  });
+}
 
 app.use(errorHandler);
 app.use(invalidPathHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
